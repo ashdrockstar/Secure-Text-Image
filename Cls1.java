c01bf6c36db2b1c3eca58a75bdf5d6de28e50513
ashdrockstar <aishwarypramanik@gmail.com>
2016-04-23 12:13:19 -0400

    Text Image Encryption

---
+++ Cls1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class Cls1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        form1 f=new form1();
+        f.setVisible(true);
+    }
+}
